46544ab9bdb7971646321c11ae1c31f47ad4faf4
dpunishe <dpunishe@yandex.ru>
2022-04-30 00:07:42 +0300

    need valid & info

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 
 <template>
 <v-app>
+  <v-card>
   <v-data-table
     :headers="headers"
     :items="row"
- 
     class="elevation-1"
+    :search="search"
   >
-  
     <template v-slot:top>
-   
-      
       <v-toolbar
         flat
       >
         <v-toolbar-title>Users</v-toolbar-title>
+         <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
         <v-divider
           class="mx-4"
           inset
@@ -117,9 +124,45 @@
             </v-card-actions>
           </v-card>
         </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this user?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-toolbar>
     </template>
+    <!-- eslint-disable-next-line -->
+     <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+  <v-data-table
+    item-key="name"
+    class="elevation-1"
+    loading
+    loading-text="Loading... Please wait"
+  ></v-data-table>
+    </template>
   </v-data-table>
+  </v-card>
 </v-app>
 </template>
 
@@ -148,6 +191,7 @@ export default {
     })
   },
     data: () => ({
+      search: '',
       dialog: false,
       dialogDelete: false,
                 headers: [
@@ -156,34 +200,39 @@ export default {
           { text: 'Last Name', value: 'lastName' },
           { text: 'Email', value: 'email' },
           { text: 'Phone', value: 'phone' },
+          { text: 'Actions', value: 'actions', sortable: false}
+          
         ],
         row: [],
       editedIndex: -1,
       editedItem: {
-        id: '',
-        firsName: 0,
-        lastName: 0,
-        email: 0,
-        phone: 0,
+        id: null,
+        firsName: null,
+        lastName: null,
+        email: null,
+        phone: null,
       },
       defaultItem: {
-        id: '',
-        firsName: 0,
-        lastName: 0,
-        email: 0,
-        phone: 0,
+        id: null,
+        firsName: null,
+        lastName: null,
+        email: null,
+        phone: null,
       },
     }),
 
     computed: {
       formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : null
+        return this.editedIndex === -1 ? 'New User' : 'Edit Item'
       },
     },
 
     watch: {
       dialog (val) {
         val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
       },
     },
 
@@ -195,8 +244,31 @@ export default {
       initialize () {
         this.row = []
       },
+      editItem (item) {
+        this.editedIndex = this.row.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = this.row.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        this.row.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
       close () {
         this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+         closeDelete () {
+        this.dialogDelete = false
         this.$nextTick(() => {
           this.editedItem = Object.assign({}, this.defaultItem)
           this.editedIndex = -1
